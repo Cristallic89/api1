06e0b2d57a0cc8899e3b9c6f955f43caeed99ba2
Kristi Rajandu <94612028+Cristallic89@users.noreply.github.com>
2022-11-22 15:38:41 +0200

    API tuli muuta ühe pasa API sarnaseks (https://m3o.com/user/api#Read)...kus kõik CRUD on POST! >.<
    Hetkel ei toimi Update.
    List, Delete ja Create toimivad.

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
-
+const NOT_FOUND_ERROR = {
+    id: "go.micro.client", code: 500, detail: "not found", status: "Internal Server Error"
+}
 app.use(express.json());
 
 //---loon users sisu...
-let users = [
-    {id: 1, username: 'machimos', email:'machimos@hot.ee', password:'SaVanaKonn123!'},
-    {id: 2, username: 'cristallic', email:'cristallic@hot.ee', password:'P0leMeeles112@'},
-    {id: 3, username: 'sasuke', email:'sasuke@hot.ee', password:'KesSeeTeab911!!!'},
-    {id: 4, username: 'hinata', email:'hinata@hot.ee', password:'Pure3nergy777#'},
-    {id: 5, username: 'playhate', email:'playhate@hot.ee', password:'SailAwayOnTuesday555¤'},
-    {id: 6, username: 'kuldmynt', email:'goldencoin@hot.ee', password:'BitCoin4ever007'},
-    {id: 7, username: 'toasted', email:'toasted@hot.ee', password:'On1Side0nly91#'},
-];
+let users = [{id: 1, username: 'machimos', email: 'machimos@hot.ee', password: 'SaVanaKonn123!'}, {
+    id: 2, username: 'cristallic', email: 'cristallic@hot.ee', password: 'P0leMeeles112@'
+}, {id: 3, username: 'sasuke', email: 'sasuke@hot.ee', password: 'KesSeeTeab911!!!'}, {
+    id: 4, username: 'hinata', email: 'hinata@hot.ee', password: 'Pure3nergy777#'
+}, {id: 5, username: 'playhate', email: 'playhate@hot.ee', password: 'SailAwayOnTuesday555¤'}, {
+    id: 6, username: 'kuldmynt', email: 'goldencoin@hot.ee', password: 'BitCoin4ever007'
+}, {id: 7, username: 'toasted', email: 'toasted@hot.ee', password: 'On1Side0nly91#'},];
 
 
 // luuakse veebilingi lisa /api/courses millele minnes kuvatakse BODY sisu...
@@ -29,42 +29,40 @@ app.post('/api/user/List', (req, res) => {
 // siis kuvatakse teave.
 app.post('/api/user/Create', (req, res) => {
     console.log(req.body);
-    const { error } = validateUser(req.body);
+    const {error} = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const user = {
-        id: users.length + 1,
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+        id: users.length + 1, username: req.body.username, email: req.body.email, password: req.body.password
     };
 
     users.push(user);
     res.send(user);
 });
 
-//UPDATE
+    //UPDATE
 app.post('/api/user/Update', (req, res) => {
-// otsib üles user'i väärtuse ja kui ei leia annab 404 errori
-    const user = users.find(c => c.id === parseInt(req.body.id));
-    if (!user) return res.status(404).send('Antud id-ga kasutajat ei ole');
+    // otsib üles user'i väärtuse ja kui ei leia annab 500 errori
+    const user = users.find(c => c.id === parseInt(req.body.id)); //enne oli req.params.id
+    if (!user) return res.status(500).send(NOT_FOUND_ERROR);
 
-    const { error } = validateUser(req.body);
+    const {error} = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-// Update user
+
+    // Update user
     user.username = req.body.username;
     user.email = req.body.email;
     user.password = req.body.password;
-    res.send(user);
+    res.send({});
 });
 
 
 // DELETE
-app.post('/api/user/Delete/:id', (req, res) => {
-    // otsib üles antud id'ga user'i. Kui ei ole olemas tuleb error 404
-    const user = users.find(c => c.id === parseInt(req.params.id));
-    if (!user) return res.status(404).send('Antud id-ga kasutajat ei ole');
+app.post('/api/user/Delete', (req, res) => {
+    // otsib üles antud id'ga user'i. Kui ei ole olemas tuleb error 500
+    const user = users.find(c => c.id === parseInt(req.body.id));
+    if (!user) return res.status(500).send(NOT_FOUND_ERROR);
 
     // kustutamine
     const index = users.indexOf(user);
@@ -77,6 +75,7 @@ app.post('/api/user/Delete/:id', (req, res) => {
 
 function validateUser(user) {
     const schema = {
+        id: Joi.number().min(1).max(Number.MAX_SAFE_INTEGER),
         username: Joi.string().min(3).required(),
         email: Joi.string().min(3).required(),
         password: Joi.string().min(3).required()
@@ -87,9 +86,9 @@ function validateUser(user) {
 
 
 //app.get('/api/users/:id', (req, res) => {
-  //  const user = users.find(c => c.id === parseInt(req.params.id));
-    //if (!user) return res.status(404).send('Antud id-ga kasutajat ei ole');
-   // res.send(user);
+//  const user = users.find(c => c.id === parseInt(req.params.id));
+//if (!user) return res.status(500).send('Antud id-ga kasutajat ei ole');
+// res.send(user);
 //});
 
 
